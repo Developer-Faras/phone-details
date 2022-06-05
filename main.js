2ad09772b5f79d22590c1b7e8c019249bb20c821
Developer-Faras <forasuddin.freelancer@gmail.com>
2022-06-06 00:14:10 +0600

    Get Phone Details Data

--- main.js
+++ main.js
@@ -48,18 +48,44 @@ function showData(data){
                             <div class="text-box"> 
                                 <h3 class="brand">Brand: ${element.brand}</h3>
                                 <h4 class="model">Model: ${element.phone_name}</h4>
-                                <p class="slug">Slug: ${element.slug}</p>
+                                <a href="#" onclick="getDetails('${element.slug}'); return false;" class="details-btn">Details</a>
                             </div>
                         </div>`;
 
             cardContainer.appendChild(div);
         });
-
-        console.log(data.data)
     }else{
         const error = new Error('No Data Found!');
         console.log(error);
         result.innerHTML = error.message;
         result.style.color = '#f3002d';
     }
+}
+
+
+
+
+// Result Details Code
+function getDetails(slug){
+
+    // Get Data By Slug From Api
+    fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
+        .then((res) => res.json())
+        .then((data) => {
+            if(data.status){
+                return data.data;
+            }else{
+                return new Error('No Data Found For This Phone');
+            }
+        })
+        .then((data) => showDetails(data))
+        .catch((err) => console.log(err));
+
+}
+
+
+// Show Details
+function showDetails(data){
+    const resultContainer = document.querySelector('.details-container');
+    console.log(data);
 }
